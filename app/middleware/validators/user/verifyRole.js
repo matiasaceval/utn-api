2187a8f909775755e71f30d7a528ff6dc7bf01c8
Matias Aceval <matiasacevalventura@gmail.com>
2022-06-11 20:02:51 -0300

    🪛 Fix: unexpected behaviour with verifyRole

--- app/middleware/validators/user/verifyRole.js
+++ app/middleware/validators/user/verifyRole.js
@@ -1,10 +1,12 @@
-const { isAdmin } = require("./userAuth")
+const status = require("../../../utils/status")
+const userRepository = require('../../../services/user/userRepository')
 
 module.exports = async(req, res, next) => {
     let { role } = req.body
 
-    if(role != undefined && role != "user"){
-        return isAdmin(req, res, next)
+    if(role !== undefined && role !== "user"){
+        const user = await userRepository.getUserById(req.userID)
+        if (user.role !== 'admin') return status.INVALID_ROLE(res)
     }
 
     next()
